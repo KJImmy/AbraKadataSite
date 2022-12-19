b8ae42201c89d354807c5ce087d31555797fcfd3
KJImmy <kurtjimhoff@gmail.com>
2022-12-18 19:10:49 -0500

    app name

--- AbraKadataSite/settings.py
+++ AbraKadataSite/settings.py
@@ -17,7 +17,7 @@ from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
