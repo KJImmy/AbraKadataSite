6a1cdd690e569ef77265ea24200a800e80dfca32
KJImmy <kurtjimhoff@gmail.com>
2022-12-29 14:37:35 -0500

    removed all

--- tiers/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from .views import format_base_view,formats_view,format_pokemon_view
-urlpatterns = [
-	path('',formats_view,name='formats'),
-	path('gen<int:generation><tier_name>/',format_base_view,name='format_base'),
-	path('gen<int:generation><tier_name>/<pokemon_unique_name>',format_pokemon_view,name='format_pokemon')
-]
