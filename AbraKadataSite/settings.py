884c843d0d2b2d0ef55ffaa9b658480aacd8ea7c
KJImmy <kurtjimhoff@gmail.com>
2023-01-02 17:06:45 -0500

    deploy full site

--- AbraKadataSite/settings.py
+++ AbraKadataSite/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # STATICFILES_DIR = [
 #         os.path.join(BASE_DIR, "static"),
 #         ]
