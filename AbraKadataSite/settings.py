ba9fec6c61adcccfda00a797420a2d1f3d35a05a
KJImmy <kurtjimhoff@gmail.com>
2023-01-03 22:08:16 -0500

    allowed hosts

--- AbraKadataSite/settings.py
+++ AbraKadataSite/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # DEBUG = False
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost,0.0.0.0").split(",")
 
 # DEVELOPMENT_MODE = True
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
