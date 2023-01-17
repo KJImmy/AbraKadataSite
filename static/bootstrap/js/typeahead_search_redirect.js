0856004da9c6f6fd7c4e056e74f243b390891a2e
KJImmy <kurtjimhoff@gmail.com>
2023-01-17 17:17:24 -0500

    fix typeaheads

--- static/bootstrap/js/typeahead_search_redirect.js
+++ static/bootstrap/js/typeahead_search_redirect.js
@@ -3,9 +3,10 @@
 // {% url 'format_pokemon' tier.generation tier.tier_name t.pokemon_unique_name %}
 
 searchRedirect = async function() {
-	console.log("Something")
+    console.log("running redirect")
 	let response = await fetch('/static/bootstrap/js/search_dict.json');
 	const search_dict = await response.json();
+    console.log("successfully retrieved search json")
 	const search = document.getElementById("search_form");
     const search_text = document.getElementById("search_input");
     if (!(search_text.value in search_dict)) {
@@ -15,9 +16,9 @@ searchRedirect = async function() {
     const redirect_string = `/formats/gen${search_dict[search_text.value][1]}${search_dict[search_text.value][2]}/${search_dict[search_text.value][0]}`
     search.action = redirect_string;
     search.submit();
-    // console.log("redirecting")
-    // console.log(search_text.value)
-    // console.log(redirect_string)
-    // console.log(search)
+    console.log("redirecting")
+    console.log(search_text.value)
+    console.log(redirect_string)
+    console.log(search)
     // return this.http.get(url,{responseType: 'text'});
 }
